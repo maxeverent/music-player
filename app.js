07be14f7af5af3904065373b8660d3b741941ed0
Maxim <maxeverent@mail.ru>
2023-03-21 14:01:55 +0300

    init

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const musicRouter = require('./music/musicRouter')
 const playlistsRouter = require('./playlists/playlistsRouter')
